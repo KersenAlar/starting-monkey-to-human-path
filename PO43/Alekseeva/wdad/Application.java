230aea4e2fb9bed4db09d1c57d7a425d8d40f02b
Ксения <kersenaleks@mail.ru>
2016-10-03 09:02:07 +0400

    Added entry point

--- PO43/Alekseeva/wdad/Application.java
+++ PO43/Alekseeva/wdad/Application.java
@@ -1,6 +1,7 @@
 package PO43.Alekseeva.wdad;
 
 public class Application {
-public staticvoid main(String[] args){
-System.out.println("«I’m Kseniya Alekseeva, and I’m not a monkey»");
-}}
+    public static void main(String[] args){
+    System.out.println("I'm Kseniya Alekseeva, and I'm not a monkey");
+    }
+}
